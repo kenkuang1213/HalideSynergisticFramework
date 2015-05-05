8bda8ac800acb967d8fdead06bc6165bb30206ab
r02922099 <kenkuang1213@gmail.com>
2015-05-05 15:45:26 +0800

    20150505

--- HelloAndroid/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/ken/src/DevicesFusion/HelloAndroid/bin/AndroidManifest.xml : \
-/home/ken/src/DevicesFusion/HelloAndroid/AndroidManifest.xml \
--- HelloAndroid/bin/RobertsOperation-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/ken/src/DevicesFusion/HelloAndroid/bin/RobertsOperation-debug-unaligned.apk : \
-/home/ken/src/DevicesFusion/HelloAndroid/bin/RobertsOperation.ap_ \
-/home/ken/src/DevicesFusion/HelloAndroid/bin/classes.dex \
-/home/ken/src/DevicesFusion/HelloAndroid/libs/armeabi/libnative.so \
-/home/ken/src/DevicesFusion/HelloAndroid/libs/armeabi-v7a/libnative.so \
